d2525160471e890864b46efd7d7332ce87a6af3b
manish511-github <76744029+manish511-github@users.noreply.github.com>
2022-10-01 14:26:17 +0530

    netflix frontend complete

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,18 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-
+import { useState } from "react";
+import { PlayArrow, Add, ThumbUpAltOutlined, ThumbDownOutlined } from "@material-ui/icons";
 
 const ItemContainer = styled.div`
-   width:225px;
-   height:120px;
-   background-color:#0b0b0b;
-   margin-right:5px;
-   overflow:hidden;
-   cursor:pointer;
-   color:white;
-
-
-
+width:225px;
+height:120px;
+background-color: #0b0b0b;
+margin-right: 5px;
+overflow: hidden;
+cursor: pointer;
+color: white;
+&:hover{
+    width:325px;
+    height: 325px;
+    position: absolute;
+    top: -150px;
+    box-shadow: 0px 0px 15px 0px rgba(255, 255, 255, 0.07);
+    border-radius: 5px;
+    img {
+        height: 140px;
+    }
+}
 `
 
 const MainImage = styled.img`
@@ -23,13 +32,108 @@ const MainImage = styled.img`
 
 
 `
+const ItemInfo = styled.div`
+
+display:flex;
+flex-direction:column;
+padding:5px;
+
+`
+const Video = styled.video`
+width:100%;
+height: 140px;
+object-fit: cover;
+position: absolute;
+top: 0;
+left: 0;
+
+
+`
+
+const IconContainer = styled.div`
+display:flex;
+margin-bottom:10px;
+.icon{
+    border:2px solid white;
+    padding:5px;
+    border-radius:50%;
+    
+}
+
+`
+const InfoTop = styled.div`
+display:flex;
+align-items: center;
+margin-bottom: 10px;
+font-size:14px;
+font-weight:600;
+color:gray;
+.limit{
+    border:1px solid gray;
+    padding:1px 3px;
+    margin:0 10px;
+
+
+}
+
+
+`
+const Description = styled.div`
+font-size:13px;
+margin-bottom:0px;
+
+
+
+
+`
+const Genre = styled.div`
+font-size: 14px;
+color: lightgray;
+`
+
 
 const MovieItem=({index})=>{
+    const[isHovered,setIsHovered]=useState(false);
+    const trailer = "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4"
     return (
-        <ItemContainer>
+        <ItemContainer style={{ left: isHovered && index * 225 - 50 + index * 2.5 }} onMouseEnter={() =>setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
 
 <MainImage src="https://cdn.cloudflare.steamstatic.com/steam/apps/1265780/capsule_616x353.jpg?t=1618237900" alt="trailer" />
+{isHovered && (
+    <>
+    <Video src={trailer} autoPlay={true} loop/>
 
+   
+    <ItemInfo>
+        <IconContainer>
+            <PlayArrow className="icon"/>
+            <Add className="icon"/>
+            <ThumbUpAltOutlined className="icon"/>
+            <ThumbDownOutlined className="icon"/>
+            
+
+        </IconContainer>
+        <InfoTop>
+            <span>2 hour 14 mins</span>
+            <span className="limit">13+</span>
+            <span>2022</span>
+
+        </InfoTop>
+        <Genre>
+        Adventure
+
+    </Genre>
+
+    <Description>
+        The lord of thedngjfkfngjknfjngfndjgdg Ringadventure game devloped by djnfjbfgjb Entairtainment.
+
+    </Description>
+    
+</ItemInfo>
+    </>
+      
+    )
+}
         </ItemContainer>
     )
 }
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,7 +1,7 @@
-import { ArrowBackIosOutlined,ArrowForwardIosOutlined } from "@material-ui/icons";
-import React from "react";
-import styled from "styled-components";
-import MovieItem from "./MovieItem";
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons'
+import React, { useRef, useState } from 'react'
+import MovieItem from './MovieItem'
+import styled from 'styled-components'
 
 const ListContainer = styled.div`
     width: 100%;
@@ -26,10 +26,10 @@ const Wrapper = styled.div`
         bottom: 0;
         margin: auto;
         cursor: pointer;
-&.left {
+        &.left {
             left: 0;
         }
-&.right {
+        &.right {
             right: 0;
         }
     }
@@ -42,33 +42,43 @@ const MovieContainer = styled.div`
     transform: translateX(0px);
     transition: all 1s ease;
 `
-const MoviesList=()=>{
-    return(
-     <ListContainer>
-<Title>Continue to Watch</Title>
-<Wrapper>
-    <ArrowBackIosOutlined className="sliderArrow left"/>
 
-        <MovieContainer>
-            <MovieItem index={0}/>
-            <MovieItem index={1}/>
-            <MovieItem index={2}/>
-            <MovieItem index={3}/>
-            <MovieItem index={4}/>
-            <MovieItem index={5}/>
-            <MovieItem index={6}/>
-            <MovieItem index={7}/>
-            <MovieItem index={8}/>
-            <MovieItem index={9}/>
+const MovieList = () => {
+    const [slideNumber, setSlideNumber] = useState(0);
+    const listRef = useRef();
 
-
-        </MovieContainer>
-        <ArrowForwardIosOutlined className="sliderArrow right"></ArrowForwardIosOutlined>
-  
-</Wrapper>
-
-
-     </ListContainer>
+    const handleClick = direction => {
+        let distance = listRef.current.getBoundingClientRect().x - 50;
+        if (direction === 'left' && slideNumber > 0) {
+            setSlideNumber(slideNumber - 1);
+            listRef.current.style.transform = `translateX(${230 + distance}px)`;
+        } 
+        if (direction === 'right' && slideNumber < 5) {
+            setSlideNumber(slideNumber + 1);
+            listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+        }
+    }
+    return (
+        <ListContainer>
+            <Title>Continue to watch</Title>
+            <Wrapper>
+                <ArrowBackIosOutlined className="sliderArrow left" onClick={() => handleClick("left")} />
+                <MovieContainer ref={listRef}>
+                    <MovieItem index={0} />
+                    <MovieItem index={1} />
+                    <MovieItem index={2} />
+                    <MovieItem index={3} />
+                    <MovieItem index={4} />
+                    <MovieItem index={5} />
+                    <MovieItem index={6} />
+                    <MovieItem index={7} />
+                    <MovieItem index={8} />
+                    <MovieItem index={9} />
+                </MovieContainer>
+                <ArrowForwardIosOutlined className="sliderArrow right" onClick={() => handleClick("right")} />
+            </Wrapper>
+        </ListContainer>
     )
 }
-export default MoviesList
+
+export default MovieList
